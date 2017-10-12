59227cbcfa467aadbcab5da4faa50d313edc31ab
Navon Francis <navonfrancis@gmail.com>
2017-10-12 16:53:35 -0400

    lit, done

--- LexicalAnalyzer/LexicalAnalyzer.c
+++ LexicalAnalyzer/LexicalAnalyzer.c
@@ -21,32 +21,40 @@ typedef enum {
 // The Lexeme list
 int lex[1000];
 
+// The Identifier list
+char id[1000][11];
+
 // Function declaritions
 void resetWord(char *word);
+int issymbol(char letter);
 
 int main() {
+
   // Data structure declarations
   int i;
+  int flag = -99;
+  int idRow = 0;
   int lexPosition = 0;
   char c;
-  char word[11];
-  char number[5];
+  char word[100]; // We handle size 11 limit later in code
+  char number[100];
+  char sym[100];
   FILE *ifp;
-
 
   // Scan in input file
   ifp = fopen("input.txt", "r");
 
   printf("Source Program: \n");
 
-
+  // Input
   while(fscanf(ifp, "%c", &c) != EOF) {
     printf("%c", c);
   }
 
   // Reset ifp
-  rewind(ifp);
+  fseek(ifp, 0, SEEK_SET);
 
+  // Set up table
   printf("\n");
   printf("Lexeme Table: \n");
   printf("Lexeme:\t\tToken Type:\n");
@@ -55,8 +63,10 @@ int main() {
   while(fscanf(ifp, "%c", &c) != EOF) {
     // Clear our word for iteration of loop
     resetWord(word);
+    resetWord(number);
+    resetWord(sym);
 
-    // Handling comments
+    // Handling comments, ignore them
     if(c == '/') {
       if(fscanf(ifp, "%c", &c) == '*') {
         while(1) {
@@ -69,6 +79,7 @@ int main() {
           }
         }
       }
+
     }
     // Handling Identifiers and Keywords
     // first char has to be alphabetic
@@ -84,15 +95,16 @@ int main() {
         // Error if identifier is too long
         if(pos > 11) {
           printf("Identifier name has exceeded size limit of 11.\n");
+          return -1;
         }
 
         fscanf(ifp, "%c", &c);
-
-        // Finally, break if there are tabs, new lines, or spaces
-        if (c == 9 || c == 10 || c == 32) {
-          break;
-        }
       }
+
+      // We currently need to rewind the ifp 1 unit back, or we will not
+      // be able to scan another symbol in.
+      fseek(ifp, -1, SEEK_CUR);
+
       // String compare to add tokens
       if(strcmp(word, "if") == 0) {
         lex[lexPosition] = ifsym;
@@ -111,8 +123,6 @@ int main() {
         lexPosition++;
 
         printf("%s\t\t%d\n", word, varsym);
-        // scan in the var here add it to lex
-
       }
       else if(strcmp(word, "end") == 0) {
         lex[lexPosition] = endsym;
@@ -186,29 +196,203 @@ int main() {
 
         printf("%s\t\t%d\n", word, nulsym);
       }
+      else if(strcmp(word, "procedure") == 0) {
+        lex[lexPosition] = procsym;
+        lexPosition++;
 
+        printf("%s\t\t%d\n", word, nulsym);
+      }
+      else {
+        lex[lexPosition] = identsym;
+        lexPosition++;
 
-      // i might have to clear word
-      // printf("%s \n", word);
+        // Add word to Lexeme List
+        printf("%s\t\t%d\n", word, identsym);
+        strcpy(id[idRow], word);
+        idRow++;
+      }
+    }
+    // Handling numbers
+    else if(isdigit(c)) {
+      int pos = 0;
+
+      while(isdigit(c)) {
+        // Build the number
+        number[pos] = c;
+        pos++;
+
+        // Handling lex error for size
+        if(pos > 5) {
+          printf("Identifier name has exceeded size limit of 5.\n");
+          return -1;
+        }
+
+        fscanf(ifp, "%c", &c);
+      }
+
+      lex[lexPosition] = numbersym;
+      lexPosition++;
+
+      // Add the number to our Lexeme List
+      printf("%s\t\t%d\n", number, numbersym);
+      strcpy(id[idRow], number);
+      idRow++;
+
+      fseek(ifp, -1, SEEK_CUR);
+    }
+    // Here we are handling symbols, rel-ops
+    // 2 char symbols include:
+    // ":="  "<="  "<>"  ">="
+    else if(!isalpha(c) || !isdigit(c)) {
+      int pos = 0;
+      sym[pos] = c;
+
+      // Equal sign
+      if(c == ':') {
+        fscanf(ifp, "%c", &c);
+
+        // handles ":="
+        if(c == '=') {
+          pos++;
+          sym[pos] = c;
+          lex[lexPosition] = becomessym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, becomessym);
+        } // handles single ':'
+        else {
+          fseek(ifp, -1, SEEK_CUR);
+          printf("This is an invalid symbol ':'\n");
+          return -1;
+        }
+      }
+      else if(c == '<') {
+        fscanf(ifp, "%c", &c);
+
+        //handles "<="
+        if(c == '=') {
+          pos++;
+          sym[pos] = c;
+          lex[lexPosition] = leqsym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, leqsym);
+        } // handles "<>"
+        else if(c == '>') {
+          pos++;
+          sym[pos] = c;
+          lex[lexPosition] = neqsym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, neqsym);
+        }
+        else {
+          fseek(ifp, -1, SEEK_CUR);
+          lex[lexPosition] = lessym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, lessym);
+        }
+      }
+      else if(c == '>') {
+        fscanf(ifp, "%c", &c);
+
+        // handles ":="
+        if(c == '=') {
+          pos++;
+          sym[pos] = c;
+          lex[lexPosition] = geqsym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, geqsym);
+        } // handles single ':'
+        else {
+          fseek(ifp, -1, SEEK_CUR);
+          lex[lexPosition] = gtrsym;
+          lexPosition++;
+
+          printf("%s\t\t%d\n", sym, gtrsym);
+        }
+      } // continue with the rest of the single char symbols
+      else if(c == '.') {
+        lex[lexPosition] = periodsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, periodsym);
+      }
+      else if(c == ';') {
+        lex[lexPosition] = semicolonsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, semicolonsym);
+      }
+      else if(c == ',') {
+        lex[lexPosition] = commasym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, commasym);
+      }
+      else if(c == '=') {
+        lex[lexPosition] = eqlsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, eqlsym);
+      }
+      else if(c == ')') {
+        lex[lexPosition] = rparentsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, rparentsym);
+      }
+      else if(c == '(') {
+        lex[lexPosition] = lparentsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, lparentsym);
+      }
+      else if(c == '*') {
+        lex[lexPosition] = multsym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, multsym);
+      }
+      else if(c == '-') {
+        lex[lexPosition] = minussym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, minussym);
+      }
+      else if(c == '+') {
+        lex[lexPosition] = plussym;
+        lexPosition++;
+
+        printf("%s\t\t%d\n", sym, plussym);
+      }
     }
   }
 
-
   // Print out Lexeme List ---------------------------------------------------->
   printf("\n");
-  printf("Lexeme List: Lex pos: %d\n", lexPosition);
+  printf("Lexeme List:\n");
+  int j = 0;
   for(i = 0; i < lexPosition; i++) {
     printf("%d ", lex[i]);
+
+    if(lex[i] == 2 || lex[i] == 3) {
+      // Print each var name or number
+      printf("%s ", id[j]);
+      j++;
+    }
   }
   printf("\n");
-  printf("%d \n", lex[0]);
 
   return 0;
 }
 
+// Function to clear out our strings, uhggg C.
 void resetWord(char *word) {
   int i;
-  for(i = 0; i < 11; i++) {
+  for(i = 0; i < 100; i++) {
     word[i] = '\0';
   }
 }
